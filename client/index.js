828f3bd814534ed59bcbb65b6eca610749601029
Chris E <14ceng@gmail.com>
2021-11-01 17:09:46 -0400

    Working Webpack Server

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>Hello, world!</div>,
+  document.getElementById('root') // make sure this is the same as the id of the div in your index.html
+);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+
+module.exports = {
+  mode: "development",
+  entry: [
+    './client/index.js'
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'client'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-react'
+          ]
+        }
+      }
+    ]
+  }
+}
