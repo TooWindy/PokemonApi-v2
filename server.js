92561efa5459d03dd44e8c2262ee66a6762b0c03
Chris E <14ceng@gmail.com>
2021-11-02 17:43:40 -0400

    Added a test database + connected it

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+const db = require('./server/db/database')
 const port = process.env.PORT || 3000; // this can be very useful if you deploy to Heroku!
 const app = require('./server/app')
 
-app.listen(port, function () {
-  console.log("Knock, knock");
-  console.log("Who's there?");
-  console.log(`Your server, listening on port ${port}`);
-})
+db.sync()
+  .then(()=> {
+    console.log("The db is synced")
+    app.listen(port, function () {
+      console.log("Knock, knock");
+      console.log("Who's there?");
+      console.log(`Your server, listening on port ${port}`);
+})})
+
