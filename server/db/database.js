069a91927c44ab9eb5ea317e04257542d536bfad
Chris E <14ceng@gmail.com>
2021-12-23 17:33:11 -0500

    Added slight database setting changes

--- server/db/database.js
+++ server/db/database.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
 
-const db = new Sequelize('postgres://localhost:5432/BoilerPlate_db', {
+const databaseName= 'pokemonPicker'
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,{
   logging: false // unless you like the logs
   // ...and there are many other options you may want to play with
 });
