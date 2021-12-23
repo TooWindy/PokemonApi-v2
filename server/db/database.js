bea3e34d083410c949d6c64876338b08e0f94dc4
Chris E <14ceng@gmail.com>
2021-12-23 17:37:32 -0500

    More setting tweaks

--- server/db/database.js
+++ server/db/database.js
@@ -1,6 +1,19 @@
 const Sequelize = require('sequelize')
 
 const databaseName= 'pokemonPicker'
+
+const config = {
+  logging: false
+};
+
+if(process.env.DATABASE_URL){
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  };
+}
+
 const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,{
   logging: false // unless you like the logs
   // ...and there are many other options you may want to play with
