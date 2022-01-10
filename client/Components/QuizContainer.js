77be3158809c366d26fdb6d5d62522979dae72f7
Chris E <14ceng@gmail.com>
2022-01-10 12:06:18 -0500

    Added quiz container + changed name of project

---
+++ client/Components/QuizContainer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const QuizContainer = () => {
+  return (
+    <div> YESSSSIRRR</div>
+  )
+}
+
+export default QuizContainer
--- client/Routes.js
+++ client/Routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Pokemon from "./Components/Pokemon"
+import QuizContainer from "./Components/QuizContainer";
 
 
 const Routes = () => {
   return (
     <div>
-      <Route path="/" component={Pokemon}/>
+      <Route exact path="/" component={Pokemon}/>
+      <Route exact path ='/quiz' component={QuizContainer}/>
     </div>
   )
 }
