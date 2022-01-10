2c77ca686b49c0ee5eb2b58af040a685459d48e0
Chris E <14ceng@gmail.com>
2022-01-10 14:22:02 -0500

    Added quiz contents. Need to add submit field

--- client/Components/QuizContainer.js
+++ client/Components/QuizContainer.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getRandomPokemon } from "../redux/pokemon";
+import QuizContent from "./QuizContent";
 
 const QuizContainer = () => {
+  const dispatch = useDispatch()
+  const pokemon = useSelector((state) => state.pokemon || [])
+
+  useEffect(() => {
+    dispatch(getRandomPokemon())
+  },[])
+
   return (
-    <div> YESSSSIRRR</div>
+    <div>
+      <h1>Name that pokemon!</h1>
+        <div className="pokemon">
+          <QuizContent pokemon={pokemon}/>
+        </div>
+      </div>
   )
 }
 
---
+++ client/Components/QuizContent.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+
+const QuizContent = (props) => {
+  const pokemon= props.pokemon[0]
+  const [answer,setAnswer] = useState(false)
+
+  //If a correct answer is given, render this
+  if(answer){
+    return (
+    <div className={'contentContainer'}>
+      {props.pokemon[0] ? (
+        <div>
+          {/* {console.log(props.pokemon[0].sprites.front_default)}
+          <img src={props.pokemon[0].sprites.front_default} className={'img'}></img> */}
+          <div>
+            <div className={"name"}>
+            {pokemon.name}
+            </div>
+              <a href={`https://bulbapedia.bulbagarden.net/wiki/${pokemon.name}_(Pok%C3%A9mon)`} target='_blank'>
+                <img src={pokemon.sprites.front_default} />
+              </a>
+            <div className="details">
+                <strong>Type:</strong> {pokemon.types.map((type) =>{
+                  return <div key={type.slot} className={type.type.name}>
+                          {" "}{type.type.name[0].toUpperCase()+type.type.name.slice(1)}{" "}
+                        </div>
+                  })}
+                  <strong>Weight:</strong>{String(pokemon.weight).slice(0,-1)+"."+ String(pokemon.weight).slice(-1)}kg
+            </div>
+            <div className="details">
+              <strong> Ability(s): </strong> {pokemon.abilities.map((ability) => {
+                  return <div key={ability.slot}>
+                          {ability.ability.name[0].toUpperCase()+ability.ability.name.slice(1)}
+                        </div>
+                  })}
+              </div>
+          </div>
+        </div>
+      ) : <div>
+            <div className={'info'}>
+             <h1>Loading....</h1>
+          </div>
+        </div>}
+    </div>
+  )
+//If an no answer or an incorrect answer has been given, render this. The only difference is that the pokemon name is hidden
+  }
+  else{
+    return(
+      <div className={'contentContainer'}>
+      {props.pokemon[0] ? (
+        <div>
+          {/* {console.log(props.pokemon[0].sprites.front_default)}
+          <img src={props.pokemon[0].sprites.front_default} className={'img'}></img> */}
+          <div>
+            <div className={"name"}>
+            ?????
+            </div>
+              <img src={pokemon.sprites.front_default} />
+            <div className="details">
+                <strong>Type:</strong> {pokemon.types.map((type) =>{
+                  return <div key={type.slot} className={type.type.name}>
+                          {" "}{type.type.name[0].toUpperCase()+type.type.name.slice(1)}{" "}
+                        </div>
+                  })}
+                  <strong>Weight:</strong>{String(pokemon.weight).slice(0,-1)+"."+ String(pokemon.weight).slice(-1)}kg
+            </div>
+            <div className="details">
+              <strong> Ability(s): </strong> {pokemon.abilities.map((ability) => {
+                  return <div key={ability.slot}>
+                          {ability.ability.name[0].toUpperCase()+ability.ability.name.slice(1)}
+                        </div>
+                  })}
+              </div>
+          </div>
+        </div>
+      ) : <div>
+            <div className={'info'}>
+             <h1>Loading....</h1>
+          </div>
+        </div>}
+    </div>
+    )
+  }
+}
+
+export default QuizContent
