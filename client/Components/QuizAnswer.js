351920af290b51325225f67fe108584c18cd2a7b
Chris E <14ceng@gmail.com>
2022-03-15 18:06:18 -0400

    Added placeholder icons and aligned them with labels

--- client/Components/QuizAnswer.js
+++ client/Components/QuizAnswer.js
@@ -2,22 +2,31 @@ import React from "react";
 
 const QuizAnswer = () => {
   return (
-    <div className= 'answerContainer'>
-      <h3>
-        Gen
-      </h3>
-      <h3>
-        Type 1
-      </h3>
-      <h3>
-        Type 2
-      </h3>
-      <h3>
-        Height
-      </h3>
-      <h3>
-        Weight
-      </h3>
+    <div>
+      <div className= 'labelContainer'>
+        <h3 className='genAlign'>
+          Gen
+        </h3>
+        <h3 className= 'type1Align'>
+          Type 1
+        </h3>
+        <h3>
+          Type 2
+        </h3>
+        <h3>
+          Height
+        </h3>
+        <h3>
+         Weight
+        </h3>
+      </div>
+      <div className="answerContainer">
+        <img src="/correctAnswer.png"/>
+        <img src="/correctAnswer.png"/>
+        <img src="/correctAnswer.png"/>
+        <img src="/correctAnswer.png"/>
+        <img src="/correctAnswer.png"/>
+      </div>
     </div>
   )
 }
